b3eb44862fad7704a4f13c2a85945d4159bde275
Illia Zyma <zyma.illia1121@vu.cdu.edu.ua>
2024-04-30 21:47:15 +0300

    fised check for philosof.

--- src/dinner_philosophers.adb
+++ src/dinner_philosophers.adb
@@ -11,7 +11,6 @@ procedure Dinner_Philosophers is
 
    task body Phylosopher is
       Id : Integer;
-      Retry_Delay : constant := 0.1;
       Id_Left_Fork, Id_Right_Fork : Integer;
    begin
       accept Start (Id : in Integer) do
@@ -24,30 +23,25 @@ procedure Dinner_Philosophers is
 
       for I in 1..10 loop
          Put_Line("Phylosopher " & Id'Img & " thinking " & I'Img & " time");
-
-         if Forks(Id_Left_Fork).Value = 1 and then Forks(Id_Right_Fork).Value = 1 then
-            Forks(Id_Left_Fork).Seize;
-            Put_Line("Phylosopher " & Id'Img & " took left fork");
-
+         if Id mod 2 = 0 then
             Forks(Id_Right_Fork).Seize;
             Put_Line("Phylosopher " & Id'Img & " took right fork");
-
-            Put_Line("Phylosopher " & Id'Img & " eating" & I'Img & " time");
-
-            Forks(Id_Right_Fork).Release;
-            Put_Line("Phylosopher " & Id'Img & " put right fork");
-
-            Forks(Id_Left_Fork).Release;
-            Put_Line("Phylosopher " & Id'Img & " put left fork");
-         else loop
-               if Forks(Id_Left_Fork).Value = 0 and then Forks(Id_Right_Fork).Value = 0 then
-                  Put_Line("Phylosopher " & Id'Img & " waiting");
-                  delay Retry_Delay;
-               else
-                  exit;
-               end if;
-            end loop;
+            Forks(Id_Left_Fork).Seize;
+            Put_Line("Phylosopher " & Id'Img & " took left fork");
+         else
+            Forks(Id_Left_Fork).Seize;
+            Put_Line("Phylosopher " & Id'Img & " took left fork");
+            Forks(Id_Right_Fork).Seize;
+            Put_Line("Phylosopher " & Id'Img & " took right fork");
          end if;
+
+         Put_Line("Phylosopher " & Id'Img & " eating" & I'Img & " time");
+
+         Forks(Id_Right_Fork).Release;
+         Put_Line("Phylosopher " & Id'Img & " put right fork");
+
+         Forks(Id_Left_Fork).Release;
+         Put_Line("Phylosopher " & Id'Img & " put left fork");
       end loop;
    end Phylosopher;
 
